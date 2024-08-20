31d4f6ce2d9a9782e320eb9d52903e8dd79b5fd8
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-08-20 17:22:29 +0300

    added the Login process , generate the token and handled the error of create token by the anootaion on line 64

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\Validator;
@@ -27,7 +28,7 @@ class AuthController extends Controller
             'password' => 'Password',
              ]);
              if ($validator->fails()) {
-                return ApiResponse::sendResponse(422,'Validation Error', $validator->messages()->all());
+                return ApiResponse::sendResponse(422,'Register Validation Error', $validator->messages()->all());
             }
             
         $user = User::create([
@@ -41,5 +42,35 @@ class AuthController extends Controller
 
         return ApiResponse::sendResponse(201, 'User created successfully', $data); 
 
+    }
+    public function login(Request $request)
+    {
+
+        $validator=Validator::make($request->all(),[
+            'email' => ['required','email','max:255'],
+            'password' => ['required'],
+        ],
+        [],
+        [
+            'email' => 'Email',
+            'password' => 'Password',
+             ]);
+             if ($validator->fails()) {
+                return ApiResponse::sendResponse(422,'Login Validation Error', $validator->errors());
+            }
+
+       if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) 
+       {
+        /** @var \App\Models\User $user **/
+
+        $user = Auth::user();
+        $data['token'] = $user->createToken('authToken')->plainTextToken;
+        $data['name'] = $user->name;
+        $data['email'] = $user->email;       
+        return ApiResponse::sendResponse(200, 'User Logged In successfully', $data); 
+       }
+       else{
+        return ApiResponse::sendResponse(401, 'Invalid credentials', []);
+       }
     }
 }
