7bc8997e6a281d5ff4c2e49688719ebc006e35d0
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-08-20 14:47:43 +0300

    paginate the Ads

---
+++ app/Http/Controllers/Api/AdController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Helpers\ApiResponse;
+use App\Models\Ad;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdRequest;
+use App\Http\Resources\AdResource;
+
+class AdController extends Controller
+{
+    public function index()
+    {
+        $ads = Ad::latest()->paginate(10);
+        if (count($ads) > 0) {
+            if ($ads->total() > $ads->perPage()) {
+                $data = [
+                    'records' => AdResource::collection($ads),
+                    'pagination links' => [
+                        'current page' => $ads->currentPage(),
+                        'per page' => $ads->perPage(),
+                        'total' => $ads->total(),
+                        'links' => [
+                            'first' => $ads->url(1),
+                            'last' => $ads->url($ads->lastPage()),
+                        ],
+                    ],
+                ];
+            } else {
+                $data = AdResource::collection($ads);
+            }
+            return ApiResponse::sendResponse(200, 'Ads Retrieved Successfully', $data);
+        }
+        return ApiResponse::sendResponse(200, 'No Ads available', []);
+    }
+
+
+}
