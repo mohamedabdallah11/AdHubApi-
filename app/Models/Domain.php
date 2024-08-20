e813c599654b9dadbf9745b3a94aa6d13bd1fe07
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-08-20 14:45:28 +0300

    handled the relation between ad and domain and require spatie/laravel-sluggable

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -2,10 +2,87 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enums\AdStatus;
+use App\Enums\DomainStatuses;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Domain extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'domains';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'status' => DomainStatuses::class,
+    ];
+
+
+    const ORDER = ['name'];
+
+    const UPLOADPATH = 'images/contracts/';
+
+    const UPLOADFIELDS = [];
+
+    public function ads()
+    {
+        return $this->hasMany(Ad::class);
+    }
+
+    public function approvedAds()
+    {
+        return $this->hasMany(Ad::class)->where('status', AdStatus::APPROVED);
+    }
+
+    public function scopePopular($query)
+    {
+        $query->where('status', '1');
+    }
+
+
 }
